894040caa68acb18d64eb6c03319f6e020f0afde
Anton-Igudesman <antonguy@gmail.com>
2023-06-21 19:27:34 -0700

    initial commit

---
+++ test/game4Test.js
@@ -0,0 +1,23 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { assert } = require('chai');
+
+describe('Game4', function () {
+  async function deployContractAndSetVariables() {
+    const Game = await ethers.getContractFactory('Game4');
+    const game = await Game.deploy();
+
+    const signer1 = ethers.provider.getSigner(0);
+
+    return { game, signer1 };
+  }
+  it('should be a winner', async function () {
+    const { game, signer1 } = await loadFixture(deployContractAndSetVariables);
+
+    // nested mappings are rough :}
+    await game.write(signer1.getAddress());
+    await game.win(signer1.getAddress());
+
+    // leave this assertion as-is
+    assert(await game.isWon(), 'You did not win the game');
+  });
+});
